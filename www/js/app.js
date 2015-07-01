c67d86f7b096e18eb3af11b464ad9b56e90196e5
Ivan Arrizabalaga <ivan.arrizabalaga@gmail.com>
2015-07-01 12:04:16 +0200

    Visualize chart

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('App', ['ionic'])
+angular.module('App', ['ionic','highcharts-ng'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('tabs', {
@@ -16,10 +16,11 @@ angular.module('App', ['ionic'])
                 }
             })
             .state('tabs.history', {
-                url: '/history',
+                url: '/history?currency',
                 views: {
                     'history-tab': {
-                        templateUrl: 'views/history/history.html'
+                        templateUrl: 'views/history/history.html',
+                        controller: 'HistoryController'
                     }
                 }
             })
